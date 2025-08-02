284028ce54a537cc2bca360ee7fcf08583eb4390
Yukino Ikegami <yknikgm@gmail.com>
2025-08-02 12:50:01 +0900

    Change test suite from nose to pytest

--- test_flati.py
+++ test_flati.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-from nose.tools import assert_equal
 import flati
 
 
 def test_flatten():
     iterable = [(1, 2, 3), (4, (5, 6))]
     expected = [1, 2, 3, 4, 5, 6]
-    assert_equal(list(flati.flatten(iterable)), expected)
+    assert list(flati.flatten(iterable)) == expected
 
     iterable = [('abc'), ('def', ('g', 'hi'))]
     expected = ['abc', 'def', 'g', 'hi']
-    assert_equal(list(flati.flatten(iterable, ignore=str)), expected)
+    assert list(flati.flatten(iterable, ignore=str)) == expected
